523e2c58e2bcb1a96d2670e350f04e3d87aa072d
Andrei <andreirech22@outlook.com>
2024-06-10 11:35:19 -0300

    fix: imports correction

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, UseGuards } from '@nestjs/common'
 import { JwtGuard } from '../auth/guard'
-import { GetUser } from 'src/auth/decorator'
+import { GetUser } from '../auth/decorator'
 import { User } from '@prisma/client'
 
 @UseGuards(JwtGuard)
