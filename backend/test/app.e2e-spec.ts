33267f2d6d35ed410cb3abebcd6bfccfd23f5699
Andrei <andreirech22@outlook.com>
2024-06-10 12:15:15 -0300

    add: test logic

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -1,24 +1,193 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { Test } from "@nestjs/testing"
+import { AppModule } from "../src/app.module"
+import { INestApplication, ValidationPipe } from "@nestjs/common"
+import { PrismaService } from "../src/prisma/prisma.service"
+import * as pactum from "pactum"
+import { AuthDto } from "src/auth/dto"
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+describe ("App e2e", () => {
+  let app: INestApplication
+  let prisma: PrismaService
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule]
+    }).compile()
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+    app = moduleRef.createNestApplication()
+    app.useGlobalPipes(new ValidationPipe({
+      whitelist: true
+    }))
+    await app.init()
+    await app.listen(3333)
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
-});
+    prisma = app.get(PrismaService)
+    await prisma.cleanDb()
+
+    pactum.request.setBaseUrl("http://localhost:3333")
+  })
+
+  afterAll(() => {
+    app.close()
+  })
+  
+  describe ("Auth", () => {
+    const dto: AuthDto = {
+      email: "test@email.com",
+      password: "123"
+    }
+
+    describe ("SignUp", () => {
+      it("Should throw if - email empty", () => {
+        return pactum
+          .spec()
+          .post("/auth/signup")
+          .withBody({password: dto.password})
+          .expectStatus(400)
+      })
+
+      it("Should throw if - password empty", () => {
+        return pactum
+          .spec()
+          .post("/auth/signup")
+          .withBody({email: dto.email})
+          .expectStatus(400)
+      })
+
+      it("Should throw if - no body", () => {
+        return pactum
+          .spec()
+          .post("/auth/signup")
+          .withBody({})
+          .expectStatus(400)
+      })
+
+      it("Should SignUp", () => {
+        return pactum
+          .spec()
+          .post("/auth/signup")
+          .withBody(dto)
+          .expectStatus(201)
+      })
+    })
+
+    describe ("SignIn", () => {
+      it("Should throw if - email empty", () => {
+        return pactum
+          .spec()
+          .post("/auth/signin")
+          .withBody({password: dto.password})
+          .expectStatus(400)
+      })
+
+      it("Should throw if - password empty", () => {
+        return pactum
+          .spec()
+          .post("/auth/signin")
+          .withBody({email: dto.email})
+          .expectStatus(400)
+      })
+
+      it("Should throw if - no body", () => {
+        return pactum
+          .spec()
+          .post("/auth/signin")
+          .withBody({})
+          .expectStatus(400)
+      })
+
+      it("Should SignIn", () => {
+        return pactum
+          .spec()
+          .post("/auth/signin")
+          .withBody(dto)
+          .expectStatus(200)
+          .stores("userAt", "access_token")
+      })
+    })
+  })
+
+  describe ("User", () => {
+    describe ("Get User", () => {
+      it("Should get current user", () => {
+        return pactum
+          .spec()
+          .get("/users/profile")
+          .withHeaders({Authorization: "Bearer $S{userAt}"})
+          .expectStatus(200)
+      })
+    })
+
+    describe ("Edit User", () => {
+      
+    })
+
+    describe ("Remove User", () => {
+      
+    })
+  })
+
+  describe ("Kanjis", () => {
+    describe ("Get Kanji", () => {
+      
+    })
+
+    describe ("Get Kanji by Id", () => {
+      
+    })
+
+    describe ("Get all Kanjis", () => {
+      
+    })
+
+    describe ("Create Kanji", () => {
+      
+    })
+
+    describe ("Create Kanji Word Association", () => {
+      
+    })
+
+    describe ("Edit Kanji", () => {
+      
+    })
+
+    describe ("Remove Kanji", () => {
+      
+    })
+  })
+
+  describe ("Words", () => {
+    describe ("Get Word", () => {
+      
+    })
+
+    describe ("Get Word by Id", () => {
+      
+    })
+
+    describe ("Get Word by Meaning", () => {
+      
+    })
+
+    describe ("Get Word by Pronunciation", () => {
+      
+    })
+
+    describe ("Get all Words", () => {
+      
+    })
+
+    describe ("Create Word", () => {
+      
+    })
+
+    describe ("Edit Word", () => {
+      
+    })
+
+    describe ("Remove Word", () => {
+      
+    })
+  })
+})
