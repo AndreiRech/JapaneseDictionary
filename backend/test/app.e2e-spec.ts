a3666c411c3cbbf969ba1e8bc951c72ec64e84c5
Andrei <andreirech22@outlook.com>
2024-06-11 11:01:25 -0300

    add: user edit test

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -1,100 +1,83 @@
-import { Test } from "@nestjs/testing"
-import { AppModule } from "../src/app.module"
-import { INestApplication, ValidationPipe } from "@nestjs/common"
-import { PrismaService } from "../src/prisma/prisma.service"
-import * as pactum from "pactum"
-import { AuthDto } from "src/auth/dto"
+import { Test } from "@nestjs/testing";
+import { AppModule } from "../src/app.module";
+import { INestApplication, ValidationPipe } from "@nestjs/common";
+import { PrismaService } from "../src/prisma/prisma.service";
+import * as pactum from "pactum";
+import { AuthDto } from "src/auth/dto";
+import { EditUserDto } from "src/user/dto";
 
-describe ("App e2e", () => {
-  let app: INestApplication
-  let prisma: PrismaService
+describe("App e2e", () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
-      imports: [AppModule]
-    }).compile()
+      imports: [AppModule],
+    }).compile();
 
-    app = moduleRef.createNestApplication()
-    app.useGlobalPipes(new ValidationPipe({
-      whitelist: true
-    }))
-    await app.init()
-    await app.listen(3333)
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
+    await app.init();
+    await app.listen(3333);
 
-    prisma = app.get(PrismaService)
-    await prisma.cleanDb()
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
 
-    pactum.request.setBaseUrl("http://localhost:3333")
-  })
+    pactum.request.setBaseUrl("http://localhost:3333");
+  });
 
   afterAll(() => {
-    app.close()
-  })
-  
-  describe ("Auth", () => {
+    app.close();
+  });
+
+  describe("Auth", () => {
     const dto: AuthDto = {
       email: "test@email.com",
-      password: "123"
-    }
+      password: "123",
+    };
 
-    describe ("SignUp", () => {
+    describe("SignUp", () => {
       it("Should throw if - email empty", () => {
         return pactum
           .spec()
           .post("/auth/signup")
-          .withBody({password: dto.password})
-          .expectStatus(400)
-      })
+          .withBody({ password: dto.password })
+          .expectStatus(400);
+      });
 
       it("Should throw if - password empty", () => {
-        return pactum
-          .spec()
-          .post("/auth/signup")
-          .withBody({email: dto.email})
-          .expectStatus(400)
-      })
+        return pactum.spec().post("/auth/signup").withBody({ email: dto.email }).expectStatus(400);
+      });
 
       it("Should throw if - no body", () => {
-        return pactum
-          .spec()
-          .post("/auth/signup")
-          .withBody({})
-          .expectStatus(400)
-      })
+        return pactum.spec().post("/auth/signup").withBody({}).expectStatus(400);
+      });
 
       it("Should SignUp", () => {
-        return pactum
-          .spec()
-          .post("/auth/signup")
-          .withBody(dto)
-          .expectStatus(201)
-      })
-    })
+        return pactum.spec().post("/auth/signup").withBody(dto).expectStatus(201);
+      });
+    });
 
-    describe ("SignIn", () => {
+    describe("SignIn", () => {
       it("Should throw if - email empty", () => {
         return pactum
           .spec()
           .post("/auth/signin")
-          .withBody({password: dto.password})
-          .expectStatus(400)
-      })
+          .withBody({ password: dto.password })
+          .expectStatus(400);
+      });
 
       it("Should throw if - password empty", () => {
-        return pactum
-          .spec()
-          .post("/auth/signin")
-          .withBody({email: dto.email})
-          .expectStatus(400)
-      })
+        return pactum.spec().post("/auth/signin").withBody({ email: dto.email }).expectStatus(400);
+      });
 
       it("Should throw if - no body", () => {
-        return pactum
-          .spec()
-          .post("/auth/signin")
-          .withBody({})
-          .expectStatus(400)
-      })
+        return pactum.spec().post("/auth/signin").withBody({}).expectStatus(400);
+      });
 
       it("Should SignIn", () => {
         return pactum
@@ -102,92 +85,73 @@ describe ("App e2e", () => {
           .post("/auth/signin")
           .withBody(dto)
           .expectStatus(200)
-          .stores("userAt", "access_token")
-      })
-    })
-  })
+          .stores("userAt", "access_token");
+      });
+    });
+  });
 
-  describe ("User", () => {
-    describe ("Get User", () => {
+  describe("User", () => {
+    describe("Get User", () => {
       it("Should get current user", () => {
         return pactum
           .spec()
           .get("/users/profile")
-          .withHeaders({Authorization: "Bearer $S{userAt}"})
+          .withHeaders({ Authorization: "Bearer $S{userAt}" })
+          .expectStatus(200);
+      });
+    });
+
+    describe("Edit User", () => {
+      it("Should edit user", () => {
+        const dto: EditUserDto = {
+          firstName: "Andrei",
+          email: "andrei123@email.com",
+        };
+        return pactum
+          .spec()
+          .patch("/users")
+          .withHeaders({ Authorization: "Bearer $S{userAt}" })
+          .withBody(dto)
           .expectStatus(200)
-      })
-    })
+          .expectBodyContains(dto.firstName)
+          .expectBodyContains(dto.email);
+      });
+    });
 
-    describe ("Edit User", () => {
-      
-    })
+    describe("Remove User", () => {});
+  });
 
-    describe ("Remove User", () => {
-      
-    })
-  })
+  describe("Kanjis", () => {
+    describe("Get Kanji", () => {});
 
-  describe ("Kanjis", () => {
-    describe ("Get Kanji", () => {
-      
-    })
+    describe("Get Kanji by Id", () => {});
 
-    describe ("Get Kanji by Id", () => {
-      
-    })
+    describe("Get all Kanjis", () => {});
 
-    describe ("Get all Kanjis", () => {
-      
-    })
+    describe("Create Kanji", () => {});
 
-    describe ("Create Kanji", () => {
-      
-    })
+    describe("Create Kanji Word Association", () => {});
 
-    describe ("Create Kanji Word Association", () => {
-      
-    })
+    describe("Edit Kanji by Id", () => {});
 
-    describe ("Edit Kanji", () => {
-      
-    })
+    describe("Remove Kanji by Id", () => {});
+  });
 
-    describe ("Remove Kanji", () => {
-      
-    })
-  })
+  describe("Words", () => {
+    describe("Get Word", () => {});
 
-  describe ("Words", () => {
-    describe ("Get Word", () => {
-      
-    })
+    describe("Get Word by Id", () => {});
 
-    describe ("Get Word by Id", () => {
-      
-    })
+    describe("Get Word by Meaning", () => {});
 
-    describe ("Get Word by Meaning", () => {
-      
-    })
+    describe("Get Word by Pronunciation", () => {});
 
-    describe ("Get Word by Pronunciation", () => {
-      
-    })
+    describe("Get all Words", () => {});
 
-    describe ("Get all Words", () => {
-      
-    })
+    describe("Create Word", () => {});
 
-    describe ("Create Word", () => {
-      
-    })
+    describe("Edit Word by Id", () => {});
 
-    describe ("Edit Word", () => {
-      
-    })
-
-    describe ("Remove Word", () => {
-      
-    })
-  })
-})
+    describe("Remove Word by Id", () => {});
+  });
+});
