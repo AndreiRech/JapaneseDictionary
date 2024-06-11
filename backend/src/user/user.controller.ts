630be2b6226a2afaf91f1b8ef0d45e60b0e86dd3
Andrei <andreirech22@outlook.com>
2024-06-11 11:00:39 -0300

    add: edit user logic

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,13 +1,22 @@
-import { Controller, Get, UseGuards } from '@nestjs/common'
-import { JwtGuard } from '../auth/guard'
-import { GetUser } from '../auth/decorator'
-import { User } from '@prisma/client'
+import { Body, Controller, Get, Patch, UseGuards } from "@nestjs/common";
+import { JwtGuard } from "../auth/guard";
+import { GetUser } from "../auth/decorator";
+import { User } from "@prisma/client";
+import { EditUserDto } from "./dto";
+import { UserService } from "./user.service";
 
 @UseGuards(JwtGuard)
 @Controller("users")
 export class UserController {
-    @Get("profile")
-    getUser(@GetUser() user: User) {
-        return user
-    }
+  constructor(private userService: UserService) {}
+
+  @Get("profile")
+  getUser(@GetUser() user: User) {
+    return user;
+  }
+
+  @Patch()
+  editUser(@GetUser("id") userId: number, @Body() dto: EditUserDto) {
+    return this.userService.editUser(userId, dto);
+  }
 }
