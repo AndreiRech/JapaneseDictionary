d574c1c1e6a03c86f6aaa7f6d3d19078b2aeb01a
Andrei <andreirech22@outlook.com>
2024-06-11 11:00:09 -0300

    fix: getUser return

--- backend/src/auth/decorator/get-user.decorator.ts
+++ backend/src/auth/decorator/get-user.decorator.ts
@@ -1,10 +1,6 @@
-import { ExecutionContext, createParamDecorator } from "@nestjs/common"
+import { ExecutionContext, createParamDecorator } from "@nestjs/common";
 
-export const GetUser= createParamDecorator(
-    (data: unknown, ctx: ExecutionContext) => {
-        const request: Express.Request = ctx
-            .switchToHttp()
-            .getRequest()
-        return request.user
-    }
-)
+export const GetUser = createParamDecorator((data: string | undefined, ctx: ExecutionContext) => {
+  const request: Express.Request = ctx.switchToHttp().getRequest();
+  return data ? request.user[data] : request.user;
+});
